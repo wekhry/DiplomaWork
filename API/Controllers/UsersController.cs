0b5c1c2da97f8f3ffcd0425ae9fde5082cfd67b0
Khrystyna Radchenko <radchenko14ks@gmail.com>
2023-04-07 16:18:03 +0300

    End of section 4

--- API/Controllers/UsersController.cs
+++ API/Controllers/UsersController.cs
@@ -1,13 +1,13 @@
 using API.Data;
 using API.Entities;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
 namespace API.Controllers
 {
-    [ApiController]
-    [Route("api/{controller}")]
-    public class UsersController : ControllerBase
+    [Authorize]
+    public class UsersController : BaseApiController
     {
         private readonly DataContext _context;
 
@@ -16,6 +16,7 @@ namespace API.Controllers
             _context = context;
         }
 
+        [AllowAnonymous]
         [HttpGet]
         //we're using ActionResult because want to return a http response with a table of users
         public async Task<ActionResult<IEnumerable<AppUser>>> GetUsers()
---
+++ API/Extensions/ApplicationServiceExtensions.cs
@@ -0,0 +1,29 @@
+using API.Data;
+using API.Interfaces;
+using API.Services;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Extensions
+{
+    public static class ApplicationServiceExtensions
+    {
+        public static IServiceCollection AddApplicationServices(this IServiceCollection services,
+        IConfiguration config)
+        {
+            services.AddDbContext<DataContext>(opt =>
+            {
+                opt.UseSqlite(config.GetConnectionString("DefaultConnection"));
+            });
+
+
+            services.AddCors();
+
+            services.AddScoped<ITokenService, TokenService>();
+
+            return services;
+        }
+    }
+
+    // when we make class static we can use the methods inside it without instantiating(creating objects) the new instance of this class 
+    // we can just use it 
+}
